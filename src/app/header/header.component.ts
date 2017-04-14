77b83403eacf9470aca5d8d1fd3b4ea500797255
Tim2.0 <B10023055@yuntech.edu.tw>
2017-04-14 11:25:45 +0800

    繫結練習

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { log } from 'util';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title = "msgm";
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  changeTitle(evt: MouseEvent){
+    this.title == "msgm.guru"?  this.title = "msgm" : this.title = "msgm.guru";
+    console.log(evt);
   }
 
 }
